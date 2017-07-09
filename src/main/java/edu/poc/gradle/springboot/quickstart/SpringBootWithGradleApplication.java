66168a5fba69d8022dfb68e36ffff6c7e71b07f4
Ritesh Bangal <rbangal@sapient.com>
2017-07-09 22:30:02 +0530

    Initial creation

---
+++ src/main/java/edu/poc/gradle/springboot/quickstart/SpringBootWithGradleApplication.java
@@ -0,0 +1,12 @@
+package edu.poc.gradle.springboot.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithGradleApplication.class, args);
+	}
+}
